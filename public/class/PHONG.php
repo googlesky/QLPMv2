6f5f06ae854395d11c0b6bd04f17ac9a975b18f3
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-23 00:01:09 +0700

    PHONG.php hoan thanh

---
+++ public/class/PHONG.php
@@ -0,0 +1,33 @@
+<?php
+	/**
+	* 
+	*/
+	class PHONG
+	{
+		var $MA_PHONG='';
+		var $TEN_PHONG='';
+		var $SLUONG_MAY='';
+		function __construct($MA_PHONG,$TEN_PHONG,$SLUONG_MAY)
+		{
+			$this->MA_PHONG=$MA_PHONG;
+			$this->TEN_PHONG=$MA_PHONG;
+			$this->SLUONG_MAY=$SLUONG_MAY;
+		}
+		//Hàm chuyển máy theo đơn vị
+		function get_DONVI(){
+
+		}
+		//Hàm quản lý thiết bị trong phòng
+		function get_THIETBI(){
+
+		}
+		//hàm cho biết công việc của phòng
+		function get_CONGVIEC(){
+
+		}
+		//hàm quản lý máy tính của phòng
+		function get_MAYTINH(){
+
+		}
+	}
+?>
