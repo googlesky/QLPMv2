d7bfdf55c64f4bcebb6ed28e7e3cc384f5fc0198
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-22 23:29:42 +0700

    DONVI.php hoan thanh

---
+++ public/class/DONVI.php
@@ -0,0 +1,18 @@
+<?php
+//khai báo class
+	/**
+	* 
+	*/
+	class DONVI 
+	{
+		var $MA_DVI='';
+		var $TEN_DVI='';
+
+		function __construct($MA_DVI,$TEN_DVI)
+		{
+			$this->MA_DVI=$MA_DVI;
+			$this->TEN_DVI=$TEN_DVI;
+		}
+		
+	}
+?>
