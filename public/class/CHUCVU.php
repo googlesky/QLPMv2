2d5db9a1c47f8efafa8439703065979e6ea991e9
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-23 00:34:43 +0700

    CHUCVU.php hoan thanh

---
+++ public/class/CHUCVU.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	* 
+	*/
+	class CHUCVU
+	{
+		var $MA_CVU='';
+		var $TEN_CVU='';
+		function __construct($MA_CVU,$TEN_CVU)
+		{
+			$this->MA_CVU=$MA_CVU;
+			$this->TEN_CVU=$TEN_CVU;
+		}
+	}
+?>
