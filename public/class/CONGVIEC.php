72cd800e40b521783043fc66bf0192cb907dd10a
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-23 00:15:25 +0700

    CONGVIEC.php hoan thanh

---
+++ public/class/CONGVIEC.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	* 
+	*/
+	class CONGVIEC
+	{
+		var $STT_CVIEC='';
+		var $NOIDUNG_CVIEC='';
+		
+		function __construct($STT_CVIEC,$NOIDUNG_CVIEC)
+		{
+			$this->STT_CVIEC=$STT_CVIEC;
+			$this->NOIDUNG_CVIEC=$NOIDUNG_CVIEC;
+		}
+	}
+?>
