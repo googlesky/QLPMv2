901eb8fafa8cd56ee1d382241b8b03759a285156
th5tuananh <lhta93@gmail.com>
2014-12-23 08:56:47 +0700

    them class MAYTINH.php

---
+++ public/class/MAYTINH.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	* LỚP MÁY TÍNH
+	*/
+	class MAYTINH
+	{
+	    public $MA_MT = 0;
+	    public $TEN_MT = '';
+	    public $NGAYCHUYEN = '';
+	    public $GHICHU_NGCHUYENMAY = '';
+
+		function __construct($MA_MT, $TEN_MT, $NGAYCHUYEN, $GHICHU_NGCHUYENMAY)
+		{
+			$this->MA_MT = $MA_MT;
+			$this->TEN_MT = $TEN_MT;
+			$this->NGAYCHUYEN = $NGAYCHUYEN;
+			$this->GHICHU_NGCHUYENMAY = $GHICHU_NGCHUYENMAY;
+		}
+
+		/**
+		 * PHƯƠNG THỨC MAYTINH
+		 * 
+		 */
+		
+
+		/**
+		 * [getMAYTINH lấy thông tin máy tính từ MA_MT]
+		 * @param  int $MA_MT 
+		 * @return [type]        [description]
+		 */
+		public function getTHONGTIN_MAYTINH($MA_MT)
+		{
+			# code...
+		}
+	}
+?>
