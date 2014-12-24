82858716a246789b41f62688b7cd10f6fc7ea66f
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-25 00:15:41 +0700

    update CONGVIEC thanh cong

--- app/lib/CONGVIEC.php
+++ app/lib/CONGVIEC.php
@@ -12,5 +12,13 @@
 			$this->STT_CVIEC=$STT_CVIEC;
 			$this->NOIDUNG_CVIEC=$NOIDUNG_CVIEC;
 		}
+		 /*Tạo công việc mới trong database*/
+	    function SaveNew()
+	    {
+	        DB::table('CONGVIEC')->insert(array(
+	            'STT_CVIEC' => $this->STT_CVIEC,
+	            'NOIDUNG_CVIEC' => $this->NOIDUNG_CVIEC
+	        ));
+	    }
 	}
 ?>
