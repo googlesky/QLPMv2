aa8cd208f49ef415d79b0a4c3f918a635b76a830
duchauIT1706 <nguyenduchau1706@gmail.com>
2014-12-25 00:03:30 +0700

    update CHUCVU done

--- app/lib/CHUCVU.php
+++ app/lib/CHUCVU.php
@@ -11,5 +11,13 @@
 			$this->MA_CVU=$MA_CVU;
 			$this->TEN_CVU=$TEN_CVU;
 		}
+		/*tạo mới chức vụ trong DB*/
+		function SaveNew()
+	    {
+	        DB::table('CHUCVU')->insert(array(
+	            'MA_CVU' => $this->MA_CVU,
+	            'TEN_CVU' => $this->TEN_ND
+	        ));
+	    }
 	}
 ?>
