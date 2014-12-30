ad31f3627b4b1a56b887c1ae568eed88aa4fdac1
googlesky <phuonghieuag@gmail.com>
2014-12-31 01:18:09 +0700

    Fix lịch, gọp nhánh cho chắc

--- app/views/phong.blade.php
+++ app/views/phong.blade.php
@@ -12,46 +12,50 @@
 @section('NoiDung')
     @include('menuheader')
     <div class="container-fluid" id="main_content">
-    @include('sidebar')
-    <div class="col-md-10 col-md-offset-2" id="main_primary">
+        @include('sidebar')
+        <div class="col-md-10 col-md-offset-2" id="main_primary">
 
-        <div class="row">
+            <div class="row">
 
-            <div class="col-md-12">
+                <div class="col-md-12">
 
-                <div class="row">
-                    <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-4">
 
-                        <h1>Quản lý phòng
-                            <small>Chi tiết về quản lý phòng</small>
-                        </h1>
+                            <h1>Quản lý phòng
+                                <small>Chi tiết về quản lý phòng</small>
+                            </h1>
+
+                        </div>
 
                     </div>
 
+
+                    <div class="alert alert-info fade in">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <strong>Chào mừng {{Session::get('USERNAME')}}</strong> bạn đã đến với chức năng quản lý
+                        phòng.
+                    </div>
+
+                    <!-- Button trigger modal -->
+
+
                 </div>
-
-
-                <div class="alert alert-info fade in">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <strong>Chào mừng {{Session::get('USERNAME')}}</strong> bạn đã đến với chức năng quản lý
-                    phòng.
-                </div>
-
-                <!-- Button trigger modal -->
-
+                <!-- End .col-md-12 -->
 
             </div>
-            <!-- End .col-md-12 -->
+            <!-- End .row top-->
 
-        </div>
-        <!-- End .row top-->
-
-        <div class="row">
-            <div class="col-md-6">
-                @include('Templates.phong.ListPhong')
+            <div class="row">
+                <div class="col-md-6">
+                    @include('Templates.phong.ListPhong')
+                </div>
+                <div class="col-md-6">
+                    @include('Templates.phong.ListPhong_InLichPhong')
+                </div>
             </div>
         </div>
-    </div> <!-- End .row tables -->
+        <!-- End .row tables -->
 
 
     </div><!-- End .col-md-10 -->
